04a302ee33f147ed8895fd13d5f9fc04a59df00a
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-04-16 13:58:17 -0300

    create a header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { PeopleComponent } from './people/people.component';
 import { PersonComponent } from './people/person/person.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [AppComponent, PeopleComponent, PersonComponent],
+  declarations: [
+    AppComponent,
+    PeopleComponent,
+    PersonComponent,
+    HeaderComponent,
+  ],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
