c73819ac9f77eb5c5b76ba23e9a17b0a7351bd99
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-04-16 11:26:26 -0300

    use property binding for drilling props

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { PeopleComponent } from './people/people.component';
+import { PersonComponent } from './people/person/person.component';
 
 @NgModule({
-  declarations: [AppComponent, PeopleComponent],
+  declarations: [AppComponent, PeopleComponent, PersonComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/people/person/person.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-person',
+  templateUrl: './person.component.html',
+})
+export class PersonComponent {
+  @Input() name: string;
+  @Input() age: number;
+}
